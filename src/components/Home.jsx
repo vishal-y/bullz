42f399ba6689eb0cfbd0e6fd6ee96adcf15ad01a
vishal yadav <heyiamvishalyadav@gmail.com>
2022-12-14 19:18:50 +0530

    first

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Typed from 'react-typed';
+
+export default function Home() {
+  return (
+    <>
+    <p>this is home</p>
+     <Typed strings={['Quicky Grow Your Money with Bullz','Get 10% return on your investment']}typeSpeed={40}/>
+    </>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React , {useState} from 'react'
+// import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai'
+import {Link} from 'react-router-dom'
+
+export default function Navbar() {
+
+    const [nav,setNav] = useState(true);
+
+    const handleNav = ()=>{
+        setNav(!nav)
+    }
+
+      const hideNav = ()=>{
+    if(!nav){
+      setNav(!nav)
+    }
+  }
+
+  return (
+    <div className='text-black flex justify-between items-center h-20 max-w-[1240px] mx-auto px-4'>
+      <div className='flex flex-row text-center items-center'>
+      <h1 className='w-full text-3xl font-bold hover:text-[#00df9a]'><Link to="/">Bullz</Link></h1>
+      <p className='border hidden md:block border-gray-50 bg-[#ddf7ea] p-1 rounded-md md:ml-6 text-[#00df9a]'>Return&nbsp;10%</p>
+      </div>
+        <ul className='md:flex hidden'>
+            <li onClick={hideNav} className='p-3'><Link to="/">Home</Link></li>
+            <li onClick={hideNav} className='p-3 ml-2'><Link to="/about">About</Link></li>
+            <li onClick={hideNav} className='p-3 ml-2'><Link to="/plan">Plan</Link></li>
+            <li onClick={hideNav} className='p-3 ml-4 text-white border hover:text-white hover:bg-[#059d6d] border-gray-50 bg-[#00df9a] text-center h-[48px] w-[7.3vw] rounded-lg'><Link to="/login">Login</Link></li>
+        </ul>
+        <div className='md:hidden hover:text-[#00df9a] flex gap-6' onClick={handleNav}>
+            {/* {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>} */}
+        </div>
+
+        <div className={!nav ? "fixed left-0 top-20 w-[100%] h-full ease-in-out duration-500 md:hidden" : "fixed -left-[100%]"}>
+            <ul className='pt-12 uppercase'>
+            <li onClick={hideNav} className='p-4 hover:text-[#00df9a] border-b border-gray-600 text-center cursor-pointer'><Link to="/">Home</Link></li>
+            <li onClick={hideNav} className='p-4 hover:text-[#00df9a] border-b border-gray-600 text-center cursor-pointer'><Link to="/about">About</Link></li>
+            <li onClick={hideNav} className='p-4 hover:text-[#00df9a] border-b border-gray-600 text-center cursor-pointer'><Link to="/plan">Plan</Link></li>
+            <li onClick={hideNav} className='p-4 h-[55px] ml-[38vw] mt-[5%] w-[22vw] text-white border border-gray-50 bg-[#00df9a] text-center  rounded-lg'><Link to="/login">Login</Link></li>
+            </ul>
+        </div>
+
+    </div>
+  )
+}
